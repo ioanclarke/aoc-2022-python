f566bf03a5154dbec72b192712c52edf5a9811a0
Ioan Clarke <Ioan.Clarke@autotrader.co.uk>
2022-12-09 20:01:49 +0000

    finish day7

--- 7/a.py
+++ 7/a.py
@@ -1,82 +1,96 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import NamedTuple
 
-with open("test.txt") as f:
-    lines = f.read().splitlines()
 
 # watch out for doing ls twice in the same directory
 
 
-tree = {}
-
-
-@dataclass
-class File:
-    name: str
+class File(NamedTuple):
+    path: str
     size: int
 
-    def of(self, ):
 
-
-@dataclass
-class Dir:
-    name: str
-    child_files: list[File]
-    child_dirs: list[Dir]
-
-    def add_child_file(self, file: File):
-        self.child_files.append(file)
-
-    def add_child_dir(self, direc: Dir):
-        self.child_dirs.append(direc)
-
-
-# We know input_line starts with '$cd'
-def get_new_cwd(input_line: str):
-    return input_line.split()[2]
+class SizedDir(NamedTuple):
+    path: str
+    size: int
+    # child_files: list[File]
+    # child_dirs: list[Dir]
+    #
+    # def add_child_file(self, file: File):
+    #     self.child_files.append(file)
+    #
+    # def add_child_dir(self, direc: Dir):
+    #     self.child_dirs.append(direc)
 
 
 def main():
-    dirs: dict[str, Dir] = {}
+    with open("input.txt") as f:
+        lines = f.read().splitlines()
+
+    dirs, files = parse_input(lines)
+    # print(f"{files=}")
+    # print("\n"*10)
+    # print(f"{dirs=}")
+    # print(f"{len(dirs)=}")
+
+    sized_dirs = find_dir_sizes(dirs, files)
+    # print(f"{sized_dirs=}")
+    total_size_of_small_dirs = sum(direc.size for direc in sized_dirs if direc.size <= 100_000)
+    # print("\n"*10)
+    # print([direc for direc in sized_dirs if direc.size <= 100_000])
+    print(total_size_of_small_dirs)
+
+
+def parse_input(lines: list[str]) -> tuple[set[str], set[File]]:
+    files = set()
+    dirs = set()
     curr_dir = "/"
     # skip initial 'cd /' line
     line_no = 1
     while line_no < len(lines):
         input_line = lines[line_no]
-        print(f"{input_line=}")
-        print(f"{curr_dir=}")
-        if input_line.startswith("$ ls"):
-            continue
-        elif input_line.startswith("$ cd"):
-            curr_dir = get_new_cwd(input_line)
-        else:
+        # print(f"{input_line=}")
+        # print(f"{curr_dir=}")
+        if input_line.startswith("$ cd"):
+            command = input_line.split()
+            arg = command[2]
+            if arg == "..":
+                curr_dir = curr_dir[:curr_dir.rfind("/")]
+                curr_dir = curr_dir[:curr_dir.rfind("/")]
+                curr_dir += "/"
+                # print(f"found '..' as arg to cd. changing curr_dir to {curr_dir}")
+            else:
+                curr_dir += arg + "/"
+                # print(f"found {arg} as arg to cd. changing curr_dir to {curr_dir}")
+
+        # This means the input_line is a dir or file
+        elif not input_line.startswith("$ ls"):
             entry = input_line.split()
-            if entry[0] == "dir":
-
-
-
-
-
-            direc = dirs.get(curr_dir, Dir(curr_dir, [], []))
-            file = get_new_f
-            curr_file = File()
-            direc.add_child_file()
+            if entry[0] != "dir":
+                file = File(curr_dir + entry[1], int(entry[0]))
+                files.add(file)
+            else:
+                dirs.add(curr_dir + entry[1])
 
         line_no += 1
 
+    return dirs, files
 
+
+def find_dir_sizes(dirs: set[str], files: set[File]) -> set[SizedDir]:
+    return {get_size(dir_name, files) for dir_name in dirs}
+
+
+def get_size(dir_name: str, files: set[File]) -> SizedDir:
+    size = 0
+    for file in files:
+        if dir_name in file.path:
+            # print(f"file {file} contains {dir_name}. adding {file.size} to sum")
+            size += file.size
+    return SizedDir(dir_name, size)
+
+
+# 517524 too low
 main()
-# print(f"Processing line >>> {input_line}")
-# if input_line == "$ ls":
-#     line_no += 1
-#     output_lines = []
-#     while line_no < len(lines):
-#         output_line = lines[line_no]
-#         print(f"Processing output line >>> {output_line}")
-#         if output_line.startswith("$"):
-#             break
-#         else:
-#             output_lines.append(output_line)
-#             line_no += 1
-#     print(f"Processing ls output: {output_lines}")
--- 7/b.py
+++ 7/b.py
@@ -1,2 +1,74 @@
-with open("input.txt") as f:
-    lines = f.read().splitlines()
+from __future__ import annotations
+
+from typing import NamedTuple
+
+
+class File(NamedTuple):
+    path: str
+    size: int
+
+
+class SizedDir(NamedTuple):
+    path: str
+    size: int
+
+
+def main():
+    with open("input.txt") as f:
+        lines = f.read().splitlines()
+
+    dirs, files = parse_input(lines)
+
+    sized_dirs = find_dir_sizes(dirs, files)
+    root = next(d for d in sized_dirs if d.path == "/")
+    total_disk_space = 70_000_000
+    space_required_for_update = 30_000_000
+    available_space = total_disk_space - root.size
+    need_to_delete = space_required_for_update - available_space
+
+    sized_dirs = sorted(list(sized_dirs), key=lambda d: d.size)
+    deletable = next(d for d in sized_dirs if d.size >= need_to_delete)
+    print(deletable.size)
+
+
+def parse_input(lines: list[str]) -> tuple[set[str], set[File]]:
+    files = set()
+    dirs = {"/"}
+    curr_dir = "/"
+    # skip initial 'cd /' line
+    line_no = 1
+    while line_no < len(lines):
+        input_line = lines[line_no]
+        if input_line.startswith("$ cd"):
+            command = input_line.split()
+            arg = command[2]
+            if arg == "..":
+                curr_dir = curr_dir[: curr_dir.rfind("/")]
+                curr_dir = curr_dir[: curr_dir.rfind("/")]
+                curr_dir += "/"
+            else:
+                curr_dir += arg + "/"
+
+        # This means the input_line is a dir or file
+        elif not input_line.startswith("$ ls"):
+            entry = input_line.split()
+            if entry[0] != "dir":
+                file = File(curr_dir + entry[1], int(entry[0]))
+                files.add(file)
+            else:
+                dirs.add(curr_dir + entry[1])
+
+        line_no += 1
+
+    return dirs, files
+
+
+def find_dir_sizes(dirs: set[str], files: set[File]) -> set[SizedDir]:
+    return {get_size(dir_name, files) for dir_name in dirs}
+
+
+def get_size(dir_name: str, files: set[File]) -> SizedDir:
+    return SizedDir(dir_name, sum(file.size for file in files if dir_name in file.path))
+
+
+main()
