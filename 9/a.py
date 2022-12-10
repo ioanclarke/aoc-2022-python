e54a38afd69a9d1c1bb1a2c3875249cfa7eff583
Ioan Clarke <Ioan.Clarke@autotrader.co.uk>
2022-12-10 15:33:47 +0000

    clean up day9a

--- 9/a.py
+++ 9/a.py
@@ -1,6 +1,10 @@
-import sys
 from enum import Enum
-from typing import NamedTuple
+from typing import NamedTuple, Callable, TypeAlias
+
+
+class Position(NamedTuple):
+    x: int
+    y: int
 
 
 class Direction(Enum):
@@ -9,26 +13,17 @@ class Direction(Enum):
     DOWN = "D"
     LEFT = "L"
 
-    @staticmethod
-    def from_str(label: str):
-        match label:
-            case "U":
-                return Direction.UP
-            case "R":
-                return Direction.RIGHT
-            case "D":
-                return Direction.DOWN
-            case "L":
-                return Direction.LEFT
-
 
 class Instruction(NamedTuple):
     direction: Direction
     steps: int
 
 
+StepHandler: TypeAlias = Callable[[Position, Position], tuple[Position, Position]]
+
+
 def main():
-    with open("test.txt") as f:
+    with open("input.txt") as f:
         lines = f.read().splitlines()
 
     insts = parse_input(lines)
@@ -39,75 +34,93 @@ def main():
 
 def parse_input(lines: list[str]) -> list[Instruction]:
     return [
-        Instruction(Direction.from_str(direction), int(steps))
+        Instruction(Direction(direction), int(steps))
         for direction, steps in (line.split() for line in lines)
     ]
 
 
-class Position(NamedTuple):
-    x: int
-    y: int
-
-
-def simulate(insts: list[Instruction]) -> list[Position]:
-    visited_positions: list[Position] = []
+def simulate(insts: list[Instruction]) -> set[Position]:
+    visited_positions: set[Position] = {Position(0, 0)}
     h_pos = Position(0, 0)
     t_pos = Position(0, 0)
     for direction, steps in insts:
-        print(f"{direction=}")
-        print(f"{steps=}")
         match direction:
             case Direction.UP:
-                # if t_pos == h_pos:
-                #     pass
-                # if t_pos.x == h_pos.x and t_pos.y == h_pos.y - 1:
-                #     pass
-                # if t_pos.x == h_pos.x + 1 and t_pos.y == h_pos.y - 1:
-                #     pass
-                if t_pos.y == h_pos.y + 1:
-                    if t_pos.x == h_pos.x - 1:
-                        t_pos = Position(t_pos.x + 1, t_pos.y - 1)
-                    elif t_pos.x == h_pos.x:
-                        t_pos = Position(t_pos.x, t_pos.y - 1)
-                    elif t_pos.x == h_pos.x + 1:
-                        t_pos = Position(t_pos.x - 1, t_pos.y - 1)
-                    else:
-                        print("SOMETHING HAS GONE HORRIBLY WRONG")
-                        sys.exit(1)
-                h_pos = Position(h_pos.x, h_pos.y - steps)
+                step_handler = handle_up_step
             case Direction.RIGHT:
-                if t_pos.x == h_pos.x - 1:
-                    """
-                    T..
-                    .H.
-                    ...
-                    """
-                    if t_pos.y == h_pos.y - 1:
-                        t_pos = Position(t_pos.x + 1, t_pos.y + 1)
-                        """
-                        ...
-                        TH.
-                        ...
-                        """
-                    elif t_pos.y == h_pos.y:
-                        t_pos = Position(t_pos.x + 1, t_pos.y)
-                        """
-                        ...
-                        .H.
-                        T..
-                        """
-                    elif t_pos.y == h_pos.y + 1:
-                        t_pos = Position(t_pos.x + 1, t_pos.y - 1)
-
-
-                h_pos = Position(h_pos.x + steps, h_pos.y)
+                step_handler = handle_right_step
             case Direction.DOWN:
-                h_pos = Position(h_pos.x, h_pos.y + steps)
+                step_handler = handle_down_step
             case Direction.LEFT:
-                h_pos = Position(h_pos.x - steps, h_pos.y)
-        print(h_pos)
-        print()
+                step_handler = handle_left_step
+        h_pos, t_pos, positions = handle_instruction(h_pos, t_pos, steps, step_handler)
+        visited_positions |= positions
     return visited_positions
+
+
+def handle_instruction(
+    h_pos: Position, t_pos: Position, steps: int, handle_step: StepHandler
+) -> tuple[Position, Position, set[Position]]:
+    visited_positions = set()
+    for _ in range(steps):
+        h_pos, t_pos = handle_step(h_pos, t_pos)
+        visited_positions.add(t_pos)
+    return h_pos, t_pos, visited_positions
+
+
+def handle_up_step(h_pos: Position, t_pos: Position) -> tuple[Position, Position]:
+    if t_pos.y == h_pos.y + 1:
+        if t_pos.x == h_pos.x - 1:
+            t_pos = Position(t_pos.x + 1, t_pos.y - 1)
+        elif t_pos.x == h_pos.x:
+            t_pos = Position(t_pos.x, t_pos.y - 1)
+        elif t_pos.x == h_pos.x + 1:
+            t_pos = Position(t_pos.x - 1, t_pos.y - 1)
+
+    h_pos = Position(h_pos.x, h_pos.y - 1)
+
+    return h_pos, t_pos
+
+
+def handle_right_step(h_pos: Position, t_pos: Position) -> tuple[Position, Position]:
+    if t_pos.x == h_pos.x - 1:
+        if t_pos.y == h_pos.y - 1:
+            t_pos = Position(t_pos.x + 1, t_pos.y + 1)
+        elif t_pos.y == h_pos.y:
+            t_pos = Position(t_pos.x + 1, t_pos.y)
+        elif t_pos.y == h_pos.y + 1:
+            t_pos = Position(t_pos.x + 1, t_pos.y - 1)
+
+    h_pos = Position(h_pos.x + 1, h_pos.y)
+
+    return h_pos, t_pos
+
+
+def handle_down_step(h_pos: Position, t_pos: Position) -> tuple[Position, Position]:
+    if t_pos.y == h_pos.y - 1:
+        if t_pos.x == h_pos.x - 1:
+            t_pos = Position(t_pos.x + 1, t_pos.y + 1)
+        elif t_pos.x == h_pos.x:
+            t_pos = Position(t_pos.x, t_pos.y + 1)
+        elif t_pos.x == h_pos.x + 1:
+            t_pos = Position(t_pos.x - 1, t_pos.y + 1)
+
+    h_pos = Position(h_pos.x, h_pos.y + 1)
+
+    return h_pos, t_pos
+
+
+def handle_left_step(h_pos: Position, t_pos: Position) -> tuple[Position, Position]:
+    if t_pos.x == h_pos.x + 1:
+        if t_pos.y == h_pos.y - 1:
+            t_pos = Position(t_pos.x - 1, t_pos.y + 1)
+        elif t_pos.y == h_pos.y:
+            t_pos = Position(t_pos.x - 1, t_pos.y)
+        elif t_pos.y == h_pos.y + 1:
+            t_pos = Position(t_pos.x - 1, t_pos.y - 1)
+
+    h_pos = Position(h_pos.x - 1, h_pos.y)
+    return h_pos, t_pos
 
 
 main()
